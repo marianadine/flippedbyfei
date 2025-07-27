868b705a5dd124c4ac01cf7b8e802ebdd9544f6c
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2025-07-27 16:20:10 +0800

    browser router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import WebController from "./controller/WebController";
 
 function App() {
   return (
-    <div style={{
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column'
-    }}>
-      <h1 style={{
-        fontFamily: 'Funnel Display, sans-serif',
-        fontSize: '48px',
-        margin: 0
-      }}>
-        Coming Soon!
-      </h1>
-      <p style={{ fontSize: '18px', marginTop: '10px' }}>
-        Stay tuned for updates.
-      </p>
-    </div>
+    <WebController />
   );
 }
 
